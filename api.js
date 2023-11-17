c9c8e8bf0aee845c02993f23415acc093768aab0
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-11-17 11:52:24 +0530

    update api.js

--- api.js
+++ api.js
@@ -1,49 +1,23 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
-const youtubeSearch = require('youtube-search');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-const youtubeSearchOptions = {
-  maxResults: 1,
-  key: 'AIzaSyD1Xa6Ohi-eig2lA7oc40HXIDiZqTyd854', // Replace with your YouTube API key
-};
 
 app.get('/download', async (req, res) => {
   try {
     const url = req.query.url;
-    const title = req.query.title;
+    const requestedQuality = req.query.quality || 'highest'; // Default to highest quality if not specified
 
-    if (!url && !title) {
-      return res.status(400).json({ error: 'URL or title parameter is missing.' });
+    if (!url) {
+      return res.status(400).json({ error: 'URL parameter is missing.' });
     }
 
-    let videoInfo;
-
-    if (url) {
-      videoInfo = await ytdl.getInfo(url);
-    } else {
-      // Search for the video based on title
-      const searchResults = await youtubeSearch(title, youtubeSearchOptions);
-      if (!searchResults || searchResults.length === 0) {
-        return res.status(404).json({ error: 'Video not found based on the provided title.' });
-      }
-
-      const firstResult = searchResults[0];
-      videoInfo = await ytdl.getInfo(firstResult.link);
-    }
-
-    // Rest of the code remains unchanged
-
-    const quality = req.query.quality || 'highest';
-    const type = req.query.type || 'video';
-
-    const formats = ytdl.filterFormats(videoInfo.formats, type);
-
-    const selectedFormat = formats.find(format => format.qualityLabel === quality);
+    const videoInfo = await ytdl.getInfo(url);
+    const formats = ytdl.filterFormats(videoInfo.formats, requestedQuality);
+    const selectedFormat = ytdl.chooseFormat(formats, { quality: requestedQuality });
 
     if (!selectedFormat) {
       return res.status(400).json({ error: 'Requested quality not available.' });
@@ -51,8 +25,8 @@ app.get('/download', async (req, res) => {
 
     const result = {
       title: videoInfo.videoDetails.title,
-      selectedQuality: selectedFormat.qualityLabel,
       downloadURL: selectedFormat.url,
+      quality: selectedFormat.qualityLabel,
     };
 
     console.log('Download result:', result);
@@ -63,7 +37,6 @@ app.get('/download', async (req, res) => {
   }
 });
 
-// Define a response for undefined routes
 app.use((req, res) => {
   res.status(404).json({ error: 'Not Found' });
 });
