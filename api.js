7486335db7225a3dfbe51ced150ac0dc2ab3b13e
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-11-22 12:48:04 +0530

    update api.js

--- api.js
+++ api.js
@@ -1,44 +1,37 @@
 const express = require('express');
-const ytdl = require('ytdl-core');
-const ffmpeg = require('fluent-ffmpeg');
-const fs = require('fs');
-
+const ytdl = require('ytdl-core-discord');
+const ytsr = require('ytsr');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/downloadurl', async (req, res) => {
+app.get('/download', async (req, res) => {
   try {
-    const query = req.query.query; // Search query or YouTube URL
+    const query = req.query.query;
 
     if (!query) {
       return res.status(400).json({ error: 'Query parameter is missing.' });
     }
 
-    let videoInfo;
-    let audioFormats;
+    const isURL = ytdl.validateURL(query);
 
-    if (ytdl.validateURL(query)) {
-      videoInfo = await ytdl.getInfo(query);
-      audioFormats = ytdl.filterFormats(videoInfo.formats, 'audio');
+    if (isURL) {
+      const audioReadableStream = await ytdl(query, { filter: 'audioonly', quality: 'highestaudio' });
+      res.setHeader('Content-Disposition', 'attachment; filename="audio.mp3"');
+      audioReadableStream.pipe(res);
     } else {
-      // Assume it's a search query
-      const searchResults = await ytdl.search(query);
-      const firstResult = searchResults.videos[0];
-      videoInfo = await ytdl.getInfo(firstResult.videoId);
-      audioFormats = ytdl.filterFormats(videoInfo.formats, 'audio');
+      const searchResults = await ytsr(query, { limit: 1 });
+      const firstVideo = searchResults.items[0];
+
+      if (!firstVideo || !firstVideo.url) {
+        return res.status(404).json({ error: 'No search results found.' });
+      }
+
+      const audioReadableStream = await ytdl(firstVideo.url, { filter: 'audioonly', quality: 'highestaudio' });
+      res.setHeader('Content-Disposition', 'attachment; filename="audio.mp3"');
+      audioReadableStream.pipe(res);
     }
-
-    const audioURL = audioFormats[0].url;
-
-    const result = {
-      title: videoInfo.videoDetails.title,
-      audioURL: audioURL,
-    };
-
-    console.log('Download result:', result);
-    res.json(result);
   } catch (error) {
     console.error('Error during download:', error);
     res.status(500).json({ error: 'An error occurred during download.' });
