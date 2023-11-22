e6192ca82101793376a3eed295689ed91cb06c6d
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-11-22 12:22:30 +0530

    update api.js

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,48 +10,26 @@ app.use(express.json());
 app.get('/download', async (req, res) => {
   try {
     const url = req.query.url; // URL ko query parameters se nikalna
-    const selectedQuality = req.query.quality; // Select kiye gaye quality ko query parameters se nikalna
 
     if (!url) {
       return res.status(400).json({ error: 'URL parameter is missing.' });
     }
 
     const videoInfo = await ytdl.getInfo(url);
-    const videoFormats = ytdl.filterFormats(videoInfo.formats, 'video');
     const audioFormats = ytdl.filterFormats(videoInfo.formats, 'audio');
 
-    if (selectedQuality) {
-      const selectedFormat = videoFormats.find((format, index) => (index + 1).toString() === selectedQuality);
+    const uniqueQualities = [...new Set(audioFormats.map((format) => format.audioQuality))];
+    const availableQualities = uniqueQualities.map((quality, index) => ({
+      number: index + 1,
+      quality: quality,
+    }));
 
-      if (!selectedFormat) {
-        return res.status(400).json({ error: 'Invalid quality parameter.' });
-      }
-
-      const audioFormat = audioFormats.find(format => format.qualityLabel === selectedFormat.qualityLabel);
-      if (!audioFormat) {
-        return res.status(400).json({ error: 'Audio not available for the selected quality.' });
-      }
-
-      const result = {
-        title: videoInfo.videoDetails.title,
-        selectedQuality: selectedFormat.qualityLabel,
-        downloadURL: selectedFormat.url,
-        audioURL: audioFormat.url,
-      };
-
-      console.log('Download result:', result);
-      return res.json(result);
-    }
-
-    const uniqueQualities = [...new Set(videoFormats.map((format) => format.qualityLabel))];
+    const audioURL = await convertToMP3(audioFormats[0].url);
 
     const result = {
       title: videoInfo.videoDetails.title,
-      availableQualities: uniqueQualities.map((quality, index) => ({
-        number: index + 1,
-        quality: quality,
-      })),
-      audioURL: audioFormats[0].url, // Pehla audio format ka URL
+      availableQualities: availableQualities,
+      audioURL: audioURL,
     };
 
     console.log('Download result:', result);
@@ -69,3 +48,14 @@ app.use((req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+async function convertToMP3(inputURL) {
+  return new Promise((resolve, reject) => {
+    ffmpeg(inputURL)
+      .audioCodec('libmp3lame')
+      .format('mp3')
+      .on('end', () => resolve(inputURL))
+      .on('error', (err) => reject(err))
+      .toBuffer();
+  });
+}
