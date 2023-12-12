b44bc3e125ad7c54af5d4954c6aea3a438a40c10
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-12-12 22:35:51 +0530

    update api.js

--- api.js
+++ api.js
@@ -1,90 +1,50 @@
 const express = require('express');
-const ytsr = require('ytsr');
-const ytdl = require('ytdl-core');
-const ffmpeg = require('fluent-ffmpeg');
-const { Readable } = require('stream');
-
+const ytdl = require('ytdl-core-discord');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.get('/downloadurl', async (req, res) => {
+// Endpoint for downloading audio by providing YouTube video URL
+app.get('/download', async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: 'Missing YouTube video URL' });
+  }
+
   try {
-    const query = req.query.query;
+    const info = await ytdl.getInfo(url);
+    const audioStream = ytdl(url, { filter: 'audioonly' });
 
-    if (!query) {
-      return res.status(400).json({ error: 'Query parameter is missing.' });
-    }
-
-    const videoInfo = await getVideoInfo(query);
-
-    if (!videoInfo) {
-      return res.status(404).json({ error: 'No search results found.' });
-    }
-
-    const audioURL = await getAudioDownloadURL(videoInfo.url);
-    const convertedAudioBuffer = await convertAudio(audioURL);
-
-    res.setHeader('Content-Type', 'audio/mpeg');
-    res.setHeader('Content-Disposition', `attachment; filename="${videoInfo.title}.mp3"`);
-    res.end(convertedAudioBuffer);
+    res.header('Content-Disposition', `attachment; filename="${info.title}.mp3"`);
+    audioStream.pipe(res);
   } catch (error) {
-    console.error('Error during download:', error);
-    res.status(500).json({ error: 'An error occurred during download.' });
+    console.error(error);
+    res.status(500).json({ error: 'Error downloading audio' });
   }
 });
 
-async function getVideoInfo(query) {
-  const searchResults = await ytsr(query, { limit: 1 });
+// Endpoint for downloading audio by providing YouTube video title
+app.get('/downloadByTitle', async (req, res) => {
+  const { title } = req.query;
 
-  if (!searchResults.items || searchResults.items.length === 0) {
-    return null;
+  if (!title) {
+    return res.status(400).json({ error: 'Missing YouTube video title' });
   }
 
-  const firstVideo = searchResults.items[0];
-  return {
-    title: firstVideo.title,
-    url: firstVideo.url,
-  };
-}
+  try {
+    const info = await ytdl.getInfo(title);
+    const audioStream = ytdl(title, { filter: 'audioonly' });
 
-async function getAudioDownloadURL(videoURL) {
-  const audioInfo = await ytdl.getInfo(videoURL);
-  const audioFormats = ytdl.filterFormats(audioInfo.formats, 'audioonly');
-
-  if (!audioFormats || audioFormats.length === 0) {
-    throw new Error('No audio formats available for the video.');
+    res.header('Content-Disposition', `attachment; filename="${info.title}.mp3"`);
+    audioStream.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error downloading audio' });
   }
-
-  const audioURL = audioFormats[0].url;
-  return audioURL;
-}
-
-async function convertAudio(audioURL) {
-  return new Promise((resolve, reject) => {
-    const audioStream = ytdl(audioURL, { quality: 'highestaudio' })
-      .on('error', (err) => reject(err));
-
-    const ffmpegCommand = ffmpeg()
-      .input(audioStream)
-      .audioCodec('libmp3lame')
-      .toFormat('mp3')
-      .on('error', (err) => reject(err));
-
-    ffmpegCommand.toBuffer((err, buffer) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(buffer);
-      }
-    });
-  });
-}
-
-// Undefined routes ke liye response define karna
-app.use((req, res) => {
-  res.status(404).json({ error: 'Not Found' });
 });
 
 app.listen(port, () => {
