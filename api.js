ae97e59be5daf953315e356243f4ee3df844b4ae
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-12-12 22:53:15 +0530

    update api.js

--- api.js
+++ api.js
@@ -1,49 +1,24 @@
 const express = require('express');
-const ytdl = require('ytdl-core-discord');
+const ytdl = require('ytdl-core');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// Endpoint for downloading audio by providing YouTube video URL
 app.get('/download', async (req, res) => {
-  const { url } = req.query;
-
-  if (!url) {
-    return res.status(400).json({ error: 'Missing YouTube video URL' });
-  }
-
   try {
+    const url = req.query.url;
+
+    if (!ytdl.validateURL(url)) {
+      throw new Error('Invalid YouTube URL');
+    }
+
     const info = await ytdl.getInfo(url);
-    const audioStream = ytdl(url, { filter: 'audioonly' });
+    const audioFormat = ytdl.filterFormats(info.formats, 'audioonly')[0];
 
     res.header('Content-Disposition', `attachment; filename="${info.title}.mp3"`);
-    audioStream.pipe(res);
+    ytdl(url, { format: audioFormat })
+      .pipe(res);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error downloading audio' });
-  }
-});
-
-// Endpoint for downloading audio by providing YouTube video title
-app.get('/downloadByTitle', async (req, res) => {
-  const { title } = req.query;
-
-  if (!title) {
-    return res.status(400).json({ error: 'Missing YouTube video title' });
-  }
-
-  try {
-    const info = await ytdl.getInfo(title);
-    const audioStream = ytdl(title, { filter: 'audioonly' });
-
-    res.header('Content-Disposition', `attachment; filename="${info.title}.mp3"`);
-    audioStream.pipe(res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error downloading audio' });
+    res.status(500).send(error.message);
   }
 });
 
