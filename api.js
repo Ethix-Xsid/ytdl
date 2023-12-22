9d3be64c70223feb1a1e3943ed1a201dc118e35d
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2023-12-22 23:15:29 +0530

    update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ytdl = require('ytdl-core');
+const ytdl = require('ytdl-core-discord');
 const ytSearch = require('yt-search');
 
 const app = express();
@@ -15,12 +15,6 @@ function formatBytes(bytes) {
   return Math.round((bytes / Math.pow(k, i)) * 100) / 100 + ' ' + sizes[i];
 }
 
-async function getAudioUrl(videoId) {
-  const info = await ytdl.getInfo(videoId);
-  const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
-  return audioFormat.url;
-}
-
 app.get('/download', async (req, res) => {
   try {
     const query = req.query.song;
@@ -32,31 +26,19 @@ app.get('/download', async (req, res) => {
     const isLink = ytdl.validateURL(query);
     const videoId = isLink ? ytdl.getURLVideoID(query) : await ytSearch(query).then(results => results.videos[0].videoId);
 
-    const downloadUrl = await getAudioUrl(videoId);
-
-    // Format duration into hours, minutes, and seconds
-    const durationHours = Math.floor(videoInfo.videoDetails.lengthSeconds / 3600);
-    const durationMinutes = Math.floor((videoInfo.videoDetails.lengthSeconds % 3600) / 60);
-    const durationSeconds = videoInfo.videoDetails.lengthSeconds % 60;
+    const downloadUrl = await ytdl.getBasicInfo(videoId).then(info => {
+      const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
+      return audioFormat.url;
+    });
 
     const result = {
-      title: videoInfo.videoDetails.title,
-      views: videoInfo.videoDetails.viewCount,
-      youtubeUrl: isLink ? query : videoInfo.videoDetails.video_url,
       downloadUrl: downloadUrl,  // Direct audio download URL
-      uploadDate: new Date(videoInfo.videoDetails.uploadDate).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }),
-      uploadChannel: videoInfo.videoDetails.author.name,
-      duration: `${durationHours}h ${durationMinutes}m ${durationSeconds}s`,
-      size: audioFormat.contentLength ? formatBytes(parseInt(audioFormat.contentLength)) : 'Unknown',
-      thumbnail: videoInfo.videoDetails.thumbnail.thumbnails[0].url,
-      likes: videoInfo.videoDetails.likes || 0,
-      dislikes: videoInfo.videoDetails.dislikes || 0,
     };
 
     console.log('Download result:', result);
 
     // Respond with JSON including the download URL for MP3
-    res.json({ downloadUrl: result.downloadUrl });
+    res.json(result);
   } catch (error) {
     console.error('Error during download:', error);
     res.status(500).json({ error: 'An error occurred during download.' });
