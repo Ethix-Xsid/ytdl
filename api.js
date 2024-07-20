ab6f9be6b0e75045a7f30c852b7bd34b53866e3f
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-07-20 22:52:41 +0530

    update api.js

--- api.js
+++ api.js
@@ -5,15 +5,10 @@ const yts = require('yt-search');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
 app.get('/download', async (req, res) => {
   const videoInput = req.query.input;
 
   try {
-    // Check if the input is a valid YouTube video URL
     if (ytdl.validateURL(videoInput)) {
       const info = await ytdl.getInfo(videoInput);
       const format = ytdl.filterFormats(info.formats, 'audioonly')[0];
@@ -21,12 +16,12 @@ app.get('/download', async (req, res) => {
       res.header('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp3"`);
       ytdl(videoInput, { format }).pipe(res);
     } else {
-      // Assume it's a search query
       const searchResults = await yts(videoInput);
       const firstVideo = searchResults.videos[0];
 
       if (firstVideo) {
-        const format = ytdl.filterFormats(firstVideo.url, 'audioonly')[0];
+        const info = await ytdl.getInfo(firstVideo.url);
+        const format = ytdl.filterFormats(info.formats, 'audioonly')[0];
 
         res.header('Content-Disposition', `attachment; filename="${firstVideo.title}.mp3"`);
         ytdl(firstVideo.url, { format }).pipe(res);
